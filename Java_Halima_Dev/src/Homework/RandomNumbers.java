cf5ece02adeb59dda0884de4db7238e20133564f
atakishiyevahalima-byte <atakishiyevahalima@gmail.com>
2025-09-20 18:57:06 +0400

    Homework Halima

---
+++ Java_Halima_Dev/src/Homework/RandomNumbers.java
@@ -0,0 +1,9 @@
+package Homework;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {11, 15, 41, 42, 123, 157, 189, 190, 195, 197};
+        System.out.println(numbers[4] + " " + numbers[5] + " " + numbers[6] +  " " + numbers[7] + " " +
+                numbers[8] + " " + numbers[9]);
+    }
+}
