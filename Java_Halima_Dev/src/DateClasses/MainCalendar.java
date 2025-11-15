bc9f9bd550cbf6b64193cbf96a7a9f03ffcfb2ae
atakishiyevahalima-byte <atakishiyevahalima@gmail.com>
2025-11-15 21:00:18 +0400

    Calendar, Date, LocalDate

---
+++ Java_Halima_Dev/src/DateClasses/MainCalendar.java
@@ -0,0 +1,31 @@
+package DateClasses;
+
+import java.util.Calendar;
+
+public class MainCalendar {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        String month[] = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "November", "December"};
+
+        System.out.println(month[calendar.get(Calendar.MONTH)]);
+        System.out.println(calendar.get(Calendar.DATE));
+        System.out.println(calendar.get(Calendar.YEAR));
+
+        System.out.println("------------------------------");
+        System.out.println("now");
+        System.out.println(calendar.get(Calendar.HOUR));
+        System.out.println(calendar.get(Calendar.MINUTE));
+        System.out.println(calendar.get(Calendar.SECOND));
+        System.out.println("------------------------------");
+        System.out.println("changed time:");
+        calendar.set(Calendar.HOUR, 12);
+        calendar.set(Calendar.MINUTE, 24);
+        calendar.set(Calendar.SECOND, 36);
+        System.out.println("changed time:");
+        System.out.println(calendar.get(Calendar.HOUR));
+        System.out.println(calendar.get(Calendar.MINUTE));
+        System.out.println(calendar.get(Calendar.SECOND));
+
+
+    }
+}
